7e136b862f946c29e47d46724febbd5897a1255f
tatsulab324 <175478545 +Tatsu-lab324@users.noreply.github.com>
2025-01-06 16:29:10 +0900

    viewsからtemplateを返す、htmlに変数を返す 2025-1-6

--- src/nippo/admin.py
+++ src/nippo/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import NippoModel
+from .models        import NippoModel
 
 admin.site.register(NippoModel)
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from random           import randint
 
-# Create your views here.
+def nippoListView(request):
+    return render(request,"nippo/nippo-list.html")
+
+def nippoDetailView(request):
+    template_name = "nippo/nippo-detail.html"
+    random_int    = randint(1,10)
+    ctx           = {
+                    "random_number" : random_int,
+                    }
+    return render(request,template_name,ctx)
